ae23507d7ed1aaeb9ecd13a5f0b7f86e4eace81d
John <john@gologonow.com>
2024-10-18 00:59:38 +0500

    code review

--- lib/View/authView/introPages.dart
+++ lib/View/authView/introPages.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
-import 'package:carousel_slider/carousel_controller.dart' as carousel;
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -16,7 +15,7 @@ class IntroPages extends StatelessWidget {
   IntroPages({Key? key}) : super(key: key);
 
   AuthController controller = Get.find();
-  final carousel.CarouselController _controller = carousel.CarouselController();
+  final CarouselSliderController _controller = CarouselSliderController();
 
   @override
   Widget build(BuildContext context) {
